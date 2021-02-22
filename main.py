ad6d467ccfb0f6655c158a1908bb434f19ed27eb
Carlos Andrés Rocha <carlos.rocha@gmail.com>
2021-02-22 01:46:29 +0000

    Add coloredlogs

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import logging
 
+import coloredlogs
 from aiohttp import web
 
 import aibo.app
 
 logger = logging.getLogger(__name__)
 
+
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s"
-    )
+    fmt = "%(asctime)s %(name)s %(levelname)s %(message)s"
+    datefmt = "%Y-%m-%d %H:%M:%S.%f"
+    coloredlogs.install(level="DEBUG", fmt=fmt, datefmt=datefmt)
     web.run_app(aibo.app.init())
