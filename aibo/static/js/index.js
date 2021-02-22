bebd7dc8b1e71282f9c20eaf44f2ed41af2f48ae
Carlos Andrés Rocha <carlos.rocha@gmail.com>
2021-02-22 04:23:00 +0000

    Create ws events on status changes

--- aibo/static/js/index.js
+++ aibo/static/js/index.js
@@ -3,24 +3,28 @@
 var status_el = document.getElementById('status');
 var events_el = document.getElementById('events');
 
+function status() {
+    var request = new XMLHttpRequest();
+    request.open('GET', '/status', true)
+    request.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+            var data = JSON.parse(this.response);
+            var text = JSON.stringify(data, null, 2);
+            status_el.textContent = text;
+        }
+    }
+    request.send()
+}
+
 function connect() {
     var ws = new WebSocket('wss://' + window.location.host + '/ws');
     
     ws.onmessage = function(event) {
         console.log(event.data);
-        data = JSON.parse(event.data)
-
-        if (data['type'] === 'status') {
-            text = JSON.stringify(data['status'], null, 2);
-            status_el.textContent = text;
-            status_el.textContent += '\n\n[' + data['time'] + ']';
-        }
-
-        if (data['type'] === 'event') {
-            text = JSON.stringify(data['event'], null, 2);
-            events_el.textContent += text + '\n\n';
-        }
-
+        var data = JSON.parse(event.data)
+        var text = JSON.stringify(data, null, 2);
+        events_el.textContent += text + '\n\n';
+        status();
     }
     
     ws.onclose = function(event) {
@@ -28,6 +32,7 @@ function connect() {
     }
 }
 
-connect()
+status();
+connect();
 
 }());
